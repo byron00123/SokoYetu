a7d0066d3453a8ef98d54d675793e73ae60cd1d9
Byron Omondi <omondibyron25@gmail.com>
2023-05-04 20:51:36 +0300

    add style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,41 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { FaShoppingCart } from 'react-icons/fa';
 
-function Navbar(props) {
+
+function Navbar() {
+  const [cartItems, setCartItems] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleSearchSubmit = (event) => {
+    event.preventDefault();
+    // handle search logic here
+  };
+
+  const handleCartClick = () => {
+    // handle cart popup logic here
+  };
+
   return (
-    <nav>
-      <div className="logo">
-        <Link to="/">SokoYetu</Link>
+    <nav className="navbar">
+      <div className="navbar__logo">
+        <img src="" alt="Sokoyetu" />
       </div>
-      <div className="cart-icon" onClick={() => props.setCartOpen(true)}>
-        <i className="fas fa-shopping-cart"></i>
-        {props.cartItems > 0 && <span>{props.cartItems}</span>}
+      <form className="navbar__search" onSubmit={handleSearchSubmit}>
+        <input
+          type="text"
+          placeholder="Search by product or category"
+          value={searchTerm}
+          onChange={handleSearchChange}
+        />
+        <button type="submit">Search</button>
+      </form>
+      <div className="navbar__cart" onClick={handleCartClick}>
+        <FaShoppingCart className="navbar__cart-icon" />
+        {cartItems.length > 0 && <div className="navbar__cart-count">{cartItems.length}</div>}
       </div>
     </nav>
   );
